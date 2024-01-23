2c0d2d9ce574d277d34c1fbd01fe1af713ea8480
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 13:27:36 +0100

    Step 5

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
-
 const day = date.getDate();
+const month = date.getMonth() + 1;
