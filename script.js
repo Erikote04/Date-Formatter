b073e71b93391b73b3ea81b94dbcc0ff108cc0e5
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 13:28:33 +0100

    Step 6

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
+const year = date.getFullYear();
