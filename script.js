42bc7a00e11707bc00c2fcc95a50d8f6eeac816e
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 13:26:31 +0100

    Step 4

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
+
+const day = date.getDate();
