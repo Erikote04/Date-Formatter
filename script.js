bbd2ddea22aac163a6a70274d0d723a6a6c2dca3
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-23 14:06:41 +0100

    Personal feature. Customized formatter fot putting a "0" before any number that is lower than 10

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
-const day = date.getDate();
-const month = date.getMonth() + 1;
+const day = date.getDate() < 10 ? "0" + (date.getDate()) : date.getDate();
+const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
 const year = date.getFullYear();
-const hours = date.getHours();
-const minutes = date.getMinutes();
+const hours = date.getHours() < 10 ? "0" + (date.getHours()) : date.getHours();
+const minutes = date.getMinutes() < 10 ? "0" + (date.getMinutes()) : date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
@@ -18,9 +18,9 @@ dateOptionsSelectElement.addEventListener("change", () => {
                 .split("-")
                 .reverse()
                 .join("-");
-            break;
+        break;
         case "mm-dd-yyyy-h-mm":
-            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+            currentDateParagraph.textContent = `${month}-${day}-${year}, ${hours}:${minutes}`;
             break;
         default:
             currentDateParagraph.textContent = formattedDate;
